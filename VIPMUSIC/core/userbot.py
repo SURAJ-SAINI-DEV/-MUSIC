7dd2d8e5566f19242b2fb0a34a4ac2cf75494cf5
SURAJ SAINI <161472575+SURAJ-SAINI-DEV@users.noreply.github.com>
2024-07-02 01:11:10 +0530

    Update userbot.py

--- VIPMUSIC/core/userbot.py
+++ VIPMUSIC/core/userbot.py
@@ -66,11 +66,11 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("THE_VIP_BOY")
-                await self.one.join_chat("THE_VIP_BOY_OP")
-                await self.one.join_chat("TG_FRIENDSS")
-                await self.one.join_chat("VIP_CREATORS")
-                await self.one.join_chat("TheTeamVivek")
+                await self.one.join_chat("quizbys")
+                await self.one.join_chat("quizbys2")
+                await self.one.join_chat("science_002")
+                await self.one.join_chat("science_02")
+                await self.one.join_chat("https://t.me/+Sjc-hUK9SBBkZTFl")
 
             except:
                 pass
@@ -97,11 +97,11 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("THE_VIP_BOY")
-                await self.two.join_chat("THE_VIP_BOY_OP")
-                await self.two.join_chat("TG_FRIENDSS")
-                await self.two.join_chat("VIP_CREATORS")
-                await self.two.join_chat("TheTeamVivek")
+                await self.two.join_chat("quizbys")
+                await self.two.join_chat("quizbys2")
+                await self.two.join_chat("science_002")
+                await self.two.join_chat("science_02")
+                await self.two.join_chat("https://t.me/+Sjc-hUK9SBBkZTFl")
             except:
                 pass
             assistants.append(2)
@@ -122,11 +122,11 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("THE_VIP_BOY")
-                await self.three.join_chat("THE_VIP_BOY_OP")
-                await self.three.join_chat("TG_FRIENDSS")
-                await self.three.join_chat("VIP_CREATORS")
-                await self.three.join_chat("TheTeamVivek")
+                await self.three.join_chat("quizbys")
+                await self.three.join_chat("quizbys2")
+                await self.three.join_chat("science_002")
+                await self.three.join_chat("science_02")
+                await self.three.join_chat("https://t.me/+Sjc-hUK9SBBkZTFl")
             except:
                 pass
             assistants.append(3)
@@ -148,11 +148,11 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("THE_VIP_BOY")
-                await self.four.join_chat("THE_VIP_BOY_OP")
-                await self.four.join_chat("TG_FRIENDSS")
-                await self.four.join_chat("VIP_CREATORS")
-                await self.four.join_chat("TheTeamVivek")
+                await self.four.join_chat("quizbys")
+                await self.four.join_chat("quizbys2")
+                await self.four.join_chat("science_002")
+                await self.four.join_chat("science_02")
+                await self.four.join_chat("https://t.me/+Sjc-hUK9SBBkZTFl")
             except:
                 pass
             assistants.append(4)
@@ -174,11 +174,11 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("THE_VIP_BOY")
-                await self.five.join_chat("THE_VIP_BOY_OP")
-                await self.five.join_chat("TG_FRIENDSS")
-                await self.five.join_chat("VIP_CREATORS")
-                await self.five.join_chat("TheTeamVivek")
+                await self.five.join_chat("quizbys")
+                await self.five.join_chat("quizbys2")
+                await self.five.join_chat("science_002")
+                await self.five.join_chat("science_02")
+                await self.five.join_chat("https://t.me/+Sjc-hUK9SBBkZTFl")
             except:
                 pass
             assistants.append(5)
